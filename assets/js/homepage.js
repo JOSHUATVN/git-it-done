e008b96d488d56e91bfe8540e0237f973cd0450a
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-04-30 20:07:41 -0500

    added some js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    console.log("function was called");
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
   };
   
-  getUserRepos();
+  getUserRepos("lernantino");
+  
